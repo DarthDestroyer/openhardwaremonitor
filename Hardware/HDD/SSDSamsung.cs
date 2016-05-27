00c93cd11b1c654d299efccb3b11288b7522f1ed
DarthDestroyer <darthdestrpyer@web.de>
2016-05-27 11:01:14 +0200

    Corrected the source code format.

--- Hardware/HDD/SSDSamsung.cs
+++ Hardware/HDD/SSDSamsung.cs
@@ -39,8 +39,8 @@ namespace OpenHardwareMonitor.Hardware.HDD {
       new SmartAttribute(0xBE, SmartNames.Temperature, 
         (byte[] r, byte v, IReadOnlyArray<IParameter> p) 
           => { return r[0] + (p == null ? 0 : p[0].Value); }, 
-          SensorType.Temperature, 0, SmartNames.Temperature, false, 
-        new[] { new ParameterDescription("Offset [°C]", 
+        SensorType.Temperature, 0, SmartNames.Temperature, false, 
+      new[] { new ParameterDescription("Offset [°C]", 
                   "Temperature offset of the thermal sensor.\n" + 
                   "Temperature = Value + Offset.", 0) }),
       new SmartAttribute(0xC2, SmartNames.AirflowTemperature),
@@ -50,7 +50,10 @@ namespace OpenHardwareMonitor.Hardware.HDD {
       new SmartAttribute(0xC9, SmartNames.SupercapStatus),
       new SmartAttribute(0xCA, SmartNames.ExceptionModeStatus),
       new SmartAttribute(0xEB, SmartNames.PowerRecoveryCount),
-      new SmartAttribute(0xF1, SmartNames.TotalLBAWritten, (byte[] r, byte v, IReadOnlyArray<IParameter> p) => { return RawToInt(r, v, p) * 512 / 1024 / 1024 / 1024; }, SensorType.Data, 0, SmartNames.TotalLBAWritten)
+      new SmartAttribute(0xF1, SmartNames.TotalLBAWritten, 
+        (byte[] r, byte v, IReadOnlyArray<IParameter> p) 
+          => { return RawToInt(r, v, p) * 512 / 1024 / 1024 / 1024; },
+        SensorType.Data, 0, SmartNames.TotalLBAWritten)
     };
 
     public SSDSamsung(ISmart smart, string name, string firmwareRevision,
